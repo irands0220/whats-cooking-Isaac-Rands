62b474901c5390019f874e3f7e86f42f6975d08c
irands0220 <167028698+irands0220@users.noreply.github.com>
2025-12-03 12:07:45 -0700

    Final Update

--- whats_cookingEDA.R
+++ whats_cookingEDA.R
@@ -1,127 +1,88 @@
-library(jsonlite)
-library(tidyverse)
-library(tidytext)
-
-# train <- read_file("/Users/isaacrands/Documents/Stats/Stat_348/whats-cooking/train.json") %>%
-# fromJSON()
-# test <- read_file("/Users/isaacrands/Documents/Stats/Stat_348/whats-cooking/test.json") %>%
-# fromJSON()
-# 
-# train_long <- train %>%
-#   unnest(ingredients)
-
-# # --- Feature Engineering ---
-# features <- train_long %>%
-#   group_by(id, cuisine) %>%
-#   
-#   summarise(
-#     # Feature 1: number of ingredients
-#     n_ingredients = n(),
-#     
-#     # Feature 2: presence/absence of key ingredients
-#     has_garlic = any(str_detect(ingredients, regex("garlic", ignore_case = TRUE))),
-#     has_soy_sauce = any(str_detect(ingredients, regex("soy sauce", ignore_case = TRUE))),
-#     has_cumin = any(str_detect(ingredients, regex("cumin", ignore_case = TRUE))),
-#     
-#     # Feature 3: number of “pepper” items
-#     n_pepper_items = sum(str_detect(ingredients, regex("pepper", ignore_case = TRUE)))
-#   ) %>%
-#   ungroup()
-
 library(tidyverse)
 library(tidymodels)
-library(embed)
-library(vroom)
-library(ranger)
-library(themis)
 library(jsonlite)
-library(tidytext)
+library(textrecipes)
+library(ranger)
+library(vroom)
 
 # --- READ DATA ---
 train <- read_file("/Users/isaacrands/Documents/Stats/Stat_348/whats-cooking/train.json") %>% fromJSON()
 test  <- read_file("/Users/isaacrands/Documents/Stats/Stat_348/whats-cooking/test.json") %>% fromJSON()
 
-# --- UNNEST ---
+# --- 1. MAKE TEXT FEATURES (WITHOUT UNNESTING) ---
+train_text <- train %>%
+  mutate(
+    ingredients_text = map_chr(ingredients, ~ paste(.x, collapse = " "))
+  ) %>%
+  select(id, cuisine, ingredients_text)
+
+test_text <- test %>%
+  mutate(
+    ingredients_text = map_chr(ingredients, ~ paste(.x, collapse = " "))
+  ) %>%
+  select(id, ingredients_text)
+
+# --- 2. MAKE NUMERIC FEATURES (NEED UNNESTED VERSION) ---
 train_long <- train %>% unnest(ingredients)
 test_long  <- test %>% unnest(ingredients)
 
-# --- FEATURE ENGINEERING FUNCTION ---
-make_features <- function(df_long, is_train = TRUE){
+make_numeric_features <- function(df_long, is_train = TRUE){
   df_long %>%
     group_by(id, !!(if (is_train) sym("cuisine") else NULL)) %>%
     summarise(
       n_ingredients = n(),
-      has_garlic = any(str_detect(ingredients, regex("garlic", ignore_case = TRUE))),
-      has_soy_sauce = any(str_detect(ingredients, regex("soy sauce", ignore_case = TRUE))),
-      has_cumin = any(str_detect(ingredients, regex("cumin", ignore_case = TRUE))),
-      n_pepper_items = sum(str_detect(ingredients, regex("pepper", ignore_case = TRUE)))
+      has_garlic = any(str_detect(ingredients, "garlic")),
+      has_soy_sauce = any(str_detect(ingredients, "soy sauce")),
+      has_cumin = any(str_detect(ingredients, "cumin")),
+      n_pepper_items = sum(str_detect(ingredients, "pepper"))
     ) %>%
     ungroup()
 }
 
-# --- BUILD FEATURE TABLES ---
-train_features <- make_features(train_long, is_train = TRUE)
-test_features  <- make_features(test_long,  is_train = FALSE)
+train_num <- make_numeric_features(train_long, TRUE)
+test_num  <- make_numeric_features(test_long, FALSE)
 
-# --- RECIPE ---
-my_recipe <- recipe(cuisine ~ ., data = train_features) %>%
-  update_role(id, new_role = "ID") %>%   # keep as ID, not predictor
+# --- 3. MERGE TEXT + NUMERIC FEATURES ---
+train_df <- left_join(train_text, train_num, by = c("id", "cuisine"))
+test_df  <- left_join(test_text, test_num,  by = "id")
+
+# --- 4. RECIPE WITH TF-IDF + NUMERIC FEATURES ---
+my_recipe <- recipe(cuisine ~ ., data = train_df) %>%
+  update_role(id, new_role = "ID") %>%
+  step_tokenize(ingredients_text) %>%
+  step_tokenfilter(ingredients_text, max_tokens = 500) %>%
+  step_tfidf(ingredients_text) %>%
   step_dummy(all_nominal_predictors()) %>%
   step_normalize(all_numeric_predictors())
 
-# --- MODEL SPEC ---
+# --- 5. RANDOM FOREST (NO TUNING) ---
 rf_model <- rand_forest(
-  mtry = tune(),
-  min_n = tune(),
-  trees = 1000
+  mtry = 20,
+  min_n = 5,
+  trees = 50
 ) %>%
   set_mode("classification") %>%
   set_engine("ranger")
 
-# --- WORKFLOW ---
 rf_wf <- workflow() %>%
-  add_model(rf_model) %>%
-  add_recipe(my_recipe)
+  add_recipe(my_recipe) %>%
+  add_model(rf_model)
 
-# --- TUNING GRID ---
-tuning_grid <- grid_regular(
-  mtry(range = c(1, 5)),
-  min_n(),
-  levels = 3
-)
+# --- 6. FIT MODEL ---
+rf_fit <- rf_wf %>% fit(train_df)
 
-# --- CV FOLDS ---
-folds <- vfold_cv(train_features, v = 10, strata = cuisine)
-
-# --- TUNE MODEL ---
-cv_results <- tune_grid(
-  rf_wf,
-  resamples = folds,
-  grid = tuning_grid,
-  metrics = metric_set(roc_auc)
-)
-
-best_tune <- cv_results %>% select_best(metric = "roc_auc")
-
-# --- FINAL FIT ---
-rf_fit <- rf_wf %>%
-  finalize_workflow(best_tune) %>%
-  fit(data = train_features)
-
-# --- PREDICT ---
-preds <- predict(rf_fit, new_data = test_features, type = "class") %>%
+# --- 7. PREDICT ---
+preds <- predict(rf_fit, test_df, type = "class") %>%
   rename(cuisine = .pred_class)
 
-# --- BIND WITH ID ---
+# --- 8. SUBMISSION ---
 submission <- bind_cols(
-  id = test_features$id,
+  id = test_df$id,
   preds
 )
 
-# --- WRITE CSV ---
 vroom_write(
   submission,
-  file = "/Users/isaacrands/Documents/Stats/Stat_348/whats-cooking/RF_whatscooking_submission.csv",
+  file = "/Users/isaacrands/Documents/Stats/Stat_348/whats-cooking/TFIDF_RF_submission.csv",
   delim = ","
 )
-
